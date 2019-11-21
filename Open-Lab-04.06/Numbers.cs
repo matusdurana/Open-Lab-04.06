52d44588aa529cd996d6fbc05bf9e49a989fc2c9
MatusDurana <matusdurana2@gmail.com>
2019-11-21 08:45:54 +0100

    Done.

--- Open-Lab-04.06/Numbers.cs
+++ Open-Lab-04.06/Numbers.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Open_Lab_04._06
 {
@@ -6,7 +7,13 @@ namespace Open_Lab_04._06
     {
         public int[] NoOdds(int[] numbers)
         {
-            throw new NotImplementedException();
+            var a = new List<int>();
+
+            foreach (var num in numbers)
+                if (num % 2 == 0)
+                    a.Add(num);
+
+            return a.ToArray();
         }
     }
 }
